ed521c462621e261714b2bd5487c92dbd4ee96ea
Tetardtek <kvnn64@gmail.com>
2023-10-12 19:35:14 +0200

    starting CSS ....

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { TetardCoinProvider } from './components/TetardCoin/TetardCoinContext';
 
 const rootElement = document.getElementById('root');
 
-createRoot(rootElement).render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <TetardCoinProvider>
       <App />
